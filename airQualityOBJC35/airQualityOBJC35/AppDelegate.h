056f3005c77651f90358934515014e4ee56f8575
Todd Crandall <toddcrandall1996@gmail.com>
2020-08-12 13:59:29 -0600

    This is my first commit.

---
+++ airQualityOBJC35/airQualityOBJC35/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  airQualityOBJC35
+//
+//  Created by Todd Crandall on 8/12/20.
+//  Copyright © 2020 Todd Crandall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ airQualityOBJC35/airQualityOBJC35/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  airQualityOBJC35
+//
+//  Created by Todd Crandall on 8/12/20.
+//  Copyright © 2020 Todd Crandall. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
